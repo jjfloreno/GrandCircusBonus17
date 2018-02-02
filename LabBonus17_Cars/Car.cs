90e5cdeca930153679c0a81941524e73a7af4978
John <jjfloreno@gmail.com>
2018-02-02 15:58:52 -0500

    First commit. Base requirements, no validation.

---
+++ LabBonus17_Cars/Car.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabBonus17_Cars
+{
+    class Car
+    {
+        //fields
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+
+        //properties
+        public string Make
+        {
+            set { make = value; }
+            get { return make; }
+        }
+
+        public string Model
+        {
+            set { model = value; }
+            get { return model; }
+        }
+
+        public int Year
+        {
+            set { year = value; }
+            get { return year; }
+        }
+
+        public double Price
+        {
+            set { price = value; }
+            get { return price; }
+        }
+
+        //constructors
+        public Car(string make, string model, int year, double price)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+        }
+
+        //method overloading (note same name as above)
+        //this is creating default objects
+        public Car() : this("Make Unknown", "Model Unknown", 0000, 0000)
+        {
+
+        }
+    }
+}
---
+++ LabBonus17_Cars/CarApp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabBonus17_Cars
+{
+    class CarApp
+    {
+        static void Main(string[] args)
+        {
+            string NumberOfCars;
+          
+            Console.Write("Welcome to Grand Circus Motors Inventory System!\n" +
+                "How many vehicles would you like to add? ");
+
+            NumberOfCars = Console.ReadLine();
+            //validate input
+
+            Car[] Inventory = new Car[int.Parse(NumberOfCars)];
+
+            for (int i = 0; i < Inventory.Length; i++)
+            {
+                Inventory[i] = new Car();
+
+                Console.Write($"Vehicle [{i + 1}] make: ");
+                //validate input
+                Inventory[i].Make = Console.ReadLine();
+
+                Console.Write($"Vehicle [{i + 1}] model: ");
+                //validate input
+                Inventory[i].Model = Console.ReadLine();
+
+                Console.Write($"Vehicle [{i + 1}] model year: ");
+                //validate year
+                Inventory[i].Year = int.Parse(Console.ReadLine());
+
+                Console.Write($"Vehicle [{i + 1}] price: ");
+                //validate price
+                Inventory[i].Price = double.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(new string('*',50));
+            Console.WriteLine("VEHICLES IN INVENTORY:");
+            Console.WriteLine(new string('*', 50));
+            Console.WriteLine($"{"MAKE",-10}{"MODEL",-10}{"YEAR",-10}{"MSRP",-10}");
+            Console.WriteLine($"{new string('-',6),-10}{new string('-', 6),-10}{new string('-', 6),-10}{new string('-', 6),-10}");
+
+            foreach (Car c in Inventory)
+            {
+                Console.WriteLine($"{c.Make,-10}{c.Model,-10}{c.Year,-10}{c.Price,-10}");
+            }
+        }
+    }
+}
